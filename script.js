4f834280e6fd162de366c6bec55b3628c5c6d632
Fernando Barrios <barrioslui02@gmail.com>
2021-08-26 22:34:10 -0500

    Add open and close modal

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 'use strict';
+// Selectors
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
-const btnOpenModal 
+const btnOpenModal = document.querySelectorAll('.show-modal');
+
+// Callback for the event listeners
+const displayModal = () => {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+  console.log('You clicked');
+};
+
+const hideModal = () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+// Iterate over the selectors to attach an event listener
+
+for (let i = 0; i < btnOpenModal.length; i++) {
+  btnOpenModal[i].addEventListener('click', displayModal);
+}
+btnCloseModal.addEventListener('click', hideModal);
